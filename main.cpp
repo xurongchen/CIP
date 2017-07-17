dc169e4a32b4f2726a6c6a8f8ed7ed7e98ef8969
Xurongchen <1459572152@qq.com>
2017-07-17 18:39:42 +0800

    test main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 #include "dbconnect.h"
+#include "test.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     createConnection();
+    test();
     MainWindow w;
     w.show();
 
